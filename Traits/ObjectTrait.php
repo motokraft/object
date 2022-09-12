4c2d981ebdbeaca779d274bcd7aa4cb50468505f
Valera Karmanov <valeravk230594@mail.ru>
2022-09-12 15:28:13 +0300

    Добавлен функционал объединения массивов и объектов

--- Traits/ObjectTrait.php
+++ Traits/ObjectTrait.php
@@ -5,6 +5,8 @@
  * @link https://github.com/motokraft/object
  */
 
+use \Motokraft\Object\BaseObject;
+
 trait ObjectTrait
 {
     private $data = [];
@@ -15,17 +17,119 @@ trait ObjectTrait
         {
             $this->set($name, $value);
         }
+
+        return $this;
     }
 
-    function set(string $name, $value) : static
+    function loadObject($object)
+    {
+        if($data = $object->getCombine())
+        {
+            $this->loadArray($data);
+        }
+
+        return $this;
+    }
+
+    function loadString(string $result)
+    {
+        $data = json_decode($result);
+
+        if(json_last_error() === JSON_ERROR_NONE)
+        {
+            $this->loadArray((array) $data);
+        }
+
+        return $this;
+    }
+
+    function mergeArray(array $data)
+    {
+        $data = new BaseObject($data);
+        return $this->mergeObject($data);
+    }
+
+    function mergeObject($object)
+    {
+        if(!$keys = $object->getKeys())
+        {
+            return $this;
+        }
+
+        foreach($keys as $name)
+        {
+            if(!$object->has($name))
+            {
+                continue;
+            }
+
+            $new_val = $object->get($name);
+            $this->set($name, $new_val);
+        }
+
+        return $this;
+    }
+
+    function getKeys() : array
+    {
+        $data = get_object_vars($this);
+
+        if(isset($data['data']))
+        {
+            unset($data['data']);
+        }
+
+        $result = (array) array_keys($data);
+        
+        if($keys = array_keys($this->data))
+        {
+            $result = array_merge($result, $keys);
+        }
+
+        $filter = function (string $name) : bool
+        {
+            return (strpos($name, '_') !== 0);
+        };
+
+        return array_filter($result, $filter);
+    }
+
+    function getValues() : array
+    {
+        $map_item = function (string $name)
+        {
+            return $this->get($name);
+        };
+
+        $keys = (array) $this->getKeys();
+        return array_map($map_item, $keys);
+    }
+
+    function getCombine() : array
+    {
+        $keys = $this->getKeys();
+        $vals = $this->getValues();
+
+        return array_combine($keys, $vals);
+    }
+
+    function getBaseObject() : BaseObject
+    {
+        $data = $this->getCombine();
+        return new BaseObject($data);
+    }
+
+    function set(string $name, $value)
     {
         if(property_exists($this, $name))
         {
             $this->{$name} = $value;
-            return $this;
+        }
+        else
+        {
+            $this->data[$name] = $value;
         }
 
-        $this->data[$name] = $value;
         return $this;
     }
 
@@ -46,19 +150,19 @@ trait ObjectTrait
 
     function remove(string $name) : bool
     {
-        if(!$this->has($name))
-        {
-            return false;
-        }
-
         if(property_exists($this, $name))
         {
             unset($this->{$name});
             return true;
         }
 
-        unset($this->data[$name]);
-        return true;
+        if(isset($this->data[$name]))
+        {
+            unset($this->data[$name]);
+            return true;
+        }
+
+        return false;
     }
 
     function has(string $name) : bool
@@ -71,9 +175,50 @@ trait ObjectTrait
         return isset($this->data[$name]);
     }
 
+    function empty(string $name) : bool
+    {
+        if(property_exists($this, $name))
+        {
+            return empty($this->{$name});
+        }
+
+        return empty($this->data[$name]);
+    }
+
     function getArray() : array
     {
         return $this->data;
+    }
+
+    function filter(callable $func) : array
+    {
+        $data = $this->getCombine();
+        $result = [];
+
+        foreach($data as $name => $value)
+        {
+            if(!$func($name, $value))
+            {
+                continue;
+            }
+
+            $result[$name] = $value;
+        }
+
+        return $result;
+    }
+
+    function map(callable $func) : array
+    {
+        $data = $this->getCombine();
+        $result = [];
+
+        foreach($data as $name => $value)
+        {
+            $result[$name] = $func($name, $value);
+        }
+
+        return $result;
     }
 
     function __get(string $name)
